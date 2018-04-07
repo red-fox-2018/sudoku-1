d98f88aac52957ccf407c62cc22c8265d76b2400
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-04-07 18:05:39 +0700

    sudoku-brama-prasetyo

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,33 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.generateBoard = []
+    this.number = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
+  }
 
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    var resultBoard = []
+    var input = 0
+    for (let i = 0; i < 9; i++) {
+      var board = []
+      for (let j = 0; j < 9; j++) {
+        if (this.number[input] == 0) {
+          board.push(' ')
+        } else {
+          board.push(this.number[input])
+        }
+        input++
+      }
+      resultBoard.push(board)
+    }
+    this.generateBoard = resultBoard
+    return resultBoard
+  }
+
 }
 
 // The file has newlines at the end of each line,
